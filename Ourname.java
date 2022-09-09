a5ecfe9a0c6b93819f2558c99606075d441d7b91
Junnalyn D. Doning <21103833@usc.edu.ph>
2022-09-09 08:28:09 +0800

    collaborator 3 added her name

--- Ourname.java
+++ Ourname.java
@@ -2,6 +2,7 @@ public class Ourname
 {
 	public static void main(String[] args) {
 		System.out.println("Soseit B. Bedoria");
-		System.out.println("Mary Grace Vellina");
+		System.out.println("Mary Grace P. Vellina");
+		System.out.println("Junnalyn D. Doning");
 	}
 }
