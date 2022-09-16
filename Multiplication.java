7993bc47a1fa4189e686bcba542fa04b91f1f636
PN-LT03\Student.admin <21103833@usc.edu.ph>
2022-09-16 19:28:36 +0800

    added multiplication

---
+++ Multiplication.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Multiplication {
+    public static void main(String[] args)
+    {
+// initializing variables
+      int sum = 0;
+    Scanner scan = new Scanner(System.in);
+// Getting input of two numbers
+        System.out.println("Enter the first number: ");
+        int num1 = scan.nextInt();
+        
+        System.out.println("Enter the second number: ");
+        int num2 = scan.nextInt();
+        
+// multiplying num1 and num2
+        product = num1 * num2;
+        
+// Displaying the output
+        System.out.println("Product = " + product);
+        
+}
+}
