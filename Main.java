1b6893354cf6854aaef9223c24e99eed3f8ba758
Soseit Bedoria <soseitbedoria@gmail.com>
2022-09-16 13:48:49 -0700

    Added main java file

---
+++ Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+  public static void main(String[] args) {
+    Flowers myObj = new Flowers();
+    myObj.setName("Jasmine");
+    myObj.setFamily("Jasminum");
+    myObj.setHealingProperties("Natural Relaxation");
+    myObj.setBotanicalName("Oleaceae");
+    
+    System.out.println("We choose "+ myObj.getName() +" flower.");
+    System.out.println("Jasmine belongs to "+ myObj.getFamily()+"family.");
+    System.out.println("Its healing properties is to give "+ myObj.getHealingProperties()+".");
+    System.out.println("And lastly, its botanical name is "+ myObj.getBotanicalName()+".");
+
+
+//object of the subclass Lily
+    Lily angiosperms = new Lily();
+// access field of superclass
+    angiosperms.mainGroup = "Monocot.";
+    angiosperms.display();
+
+// call method of superclass
+// using object of subclass
+    angiosperms.roots();
+// Overloading
+    myObj.leaf("parallel veins.");    
+   }
+}
+  
+
+
+           
