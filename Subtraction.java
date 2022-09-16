4b927124af1d60cd911f063505264914158a3b45
PN-LT03\Student.admin <marygracevellina@gmail.com>
2022-09-16 18:22:51 +0800

    Added Subtraction.java

---
+++ Subtraction.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Subraction {
+    public static void main(String[] args)
+    {
+// initializing variables
+      int sum = 0;
+    Scanner scan = new Scanner(System.in);
+// Getting input of two numbers
+        System.out.println("Enter the first number: ");
+        int num1 = scan.nextInt();
+        
+        System.out.println("Enter the second number: ");
+        int num2 = scan.nextInt();
+        
+// subtracting num1 and num2
+        difference = num1 - num2;
+        
+// Displaying the output
+        System.out.println("Difference = " + difference);
+        
+}
+}
