667489b2c1b36c68ae185adc4e2fea332288de8e
marygracevellina123 <marygracevellina@gmail.com>
2022-09-16 17:09:33 +0800

    delete

--- Ourname.java
+++
@@ -1,8 +0,0 @@
-public class Ourname
-{
-	public static void main(String[] args) {
-		System.out.println("Soseit B. Bedoria");
-		System.out.println("Mary Grace P. Vellina");
-		System.out.println("Junnalyn D. Doning");
-	}
-}
