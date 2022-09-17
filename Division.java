4fa4a67fd6a72bd4442d7d5360e8ae599e635bac
Soseit Bedoria <soseitbedoria@gmail.com>
2022-09-16 18:03:55 -0700

    Added division java file.

---
+++ Division.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Division {
+    public static void main(String[] args)
+    {
+// initializing variables
+      int sum = 0;
+    Scanner scan = new Scanner(System.in);
+// Getting input of two numbers
+        System.out.println("Enter the first number: ");
+        int num1 = scan.nextInt();
+        
+        System.out.println("Enter the second number: ");
+        int num2 = scan.nextInt();
+        
+// dividing num1 and num2
+        quotient = num1 / num2;
+        
+// Displaying the output
+        System.out.println("Product = " + quotient);
+        
+}
+}
