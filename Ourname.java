ed0ca91e04b0a7e0ae6bcd1e882134a6650a629c
Soseit Bedoria <soseitbedoria@gmail.com>
2022-09-16 21:47:34 -0700

    Name

---
+++ Ourname.java
@@ -0,0 +1,8 @@
+public class Ourname
+{
+	public static void main(String[] args) {
+		System.out.println("Soseit B. Bedoria");
+		System.out.println("Mary Grace P. Vellina");
+		System.out.println("Junnalyn D. Doning");
+	}
+}
