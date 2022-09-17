03f1da55bd91b7598532728e92f4696747265b86
Mary Grace Vellina <21103842@usc.edu.ph>
2022-09-17 08:43:56 +0800

    Added my name

--- Ourname.java
+++ Ourname.java
@@ -2,7 +2,7 @@ public class Ourname
 {
 	public static void main(String[] args) {
 		System.out.println("Soseit Bedoria");
-		System.out.println("Mary Grace P. Vellina");
+		System.out.println("Mary Grace Vellina");
 		System.out.println("Junnalyn Doning");
 	}
 }
